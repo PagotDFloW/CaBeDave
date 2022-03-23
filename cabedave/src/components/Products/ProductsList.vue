192a19494bff14d33fd2f5088156f5de4b3db3aa
Gabrielle20 <gabriellecaeiro@gmail.com>
2022-03-23 12:49:41 +0100

    home page + nav

---
+++ cabedave/src/components/Products/ProductsList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <v-container>
+        <v-row>
+            <v-col sm="6" lg="4">
+            <!--<v-text-field label="Find"
+                          prepend-icon="mdi-magnify"
+                          v-model="nameFilter">
+            </v-text-field>-->
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col v-for="(product, index) in data" :key="index" lg="4" sm="6">
+            <product :product="product"/>
+
+            </v-col>
+        </v-row>
+        </v-container>
+    </div>
+</template>
+
+<script>
+// eslint-disable-next-line import/extensions
+import Product from '@/components/Products/Product.vue';
+
+export default {
+  name: 'ProductsList',
+  components: { Product },
+  data() {
+    return {
+      data: [],
+      nameFilter: '',
+    };
+  },
+};
+</script>
---
+++ cabedave/src/views/ProductsView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <products-list/>
+    </div>
+</template>
+
+<script>
+import ProductsList from '../components/Products/ProductsList.vue';
+
+export default {
+  name: 'ProductsView',
+  components: {
+    ProductsList,
+  },
+};
+</script>
