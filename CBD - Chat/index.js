5fa7a84c6d686d91ee6e16f85fe51620d02c5848
PagotDFloW <webflopgt@gmail.com>
2022-03-23 10:00:19 +0100

    update package.json

---
+++ CBD - Chat/index.js
@@ -0,0 +1,12 @@
+﻿const express = require("express");
+const http = require("http")
+const app = express();
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const  io = new Server( server, {
+    cors: {
+        origin: '*',
+    }
+} )
+const cors = require('cors')
+app.use(cors())
