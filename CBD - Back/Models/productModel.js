b0416214eb32881c6cae6cd86e694b68ef6fb84a
Lily <lilybelhocine006@gmail.com>
2022-03-24 14:14:56 +0100

    add,delete,update product

---
+++ CBD - Back/Models/productModel.js
@@ -0,0 +1,15 @@
+﻿const mongoose = require("../dataBase.js");
+
+// create an schema
+const productSchema = new mongoose.Schema({
+    ref:String,
+    name:String,
+    description:String,
+    categories: String,
+    price:Number,
+    attributs: String,
+});
+
+const productModel = mongoose.model('products', productSchema);
+
+module.exports = { productModel }
